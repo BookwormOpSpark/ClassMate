74fb8c27ee9f49810926c78ea76d7ca7c156114f
Aurelie Lebec <aurelie.lebec@gmail.com>
2018-01-14 23:43:21 -0600

    [redux2]

--- App.js
+++ App.js
@@ -1,67 +1,16 @@
 import React from 'react';
-import { Animated } from 'react-native';
-import { TabNavigator } from 'react-navigation';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import LoginNavigation from './app/navigation/LoginNavigation';
-import StudentClassNavigation from './app/navigation/StudentClassNavigation';
-import StudentDashboardNavigation from './app/navigation/StudentDashboardNavigation';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './app/reducers';
+import MainNavigation from './app/navigation/MainNavigation';
 
 
-const TabAnimations = TabNavigator(
-  {
-    MainTab: {
-      screen: LoginNavigation,
-      path: '/',
-      navigationOptions: {
-        tabBarLabel: 'FirstPage',
-        tabBarIcon: ({ tintColor, focused }) => (
-          <Ionicons
-            name={focused ? 'ios-home' : 'ios-home-outline'}
-            size={15}
-            style={{ color: tintColor }}
-          />
-        ),
-      },
-    },
-    SettingsTab: {
-      screen: StudentDashboardNavigation,
-      path: '/',
-      navigationOptions: {
-        tabBarLabel: 'StudentDashb',
-        tabBarIcon: ({ tintColor, focused }) => (
-          <Ionicons
-            name={focused ? 'ios-settings' : 'ios-settings-outline'}
-            size={15}
-            style={{ color: tintColor }}
-          />
-        ),
-      },
-    },
-    ClassTab: {
-      screen: StudentClassNavigation,
-      path: '/',
-      navigationOptions: {
-        tabBarLabel: 'StudClassDashb',
-        tabBarIcon: ({ tintColor, focused }) => (
-          <Ionicons
-            name={focused ? 'ios-settings' : 'ios-settings-outline'}
-            size={15}
-            style={{ color: tintColor }}
-          />
-        ),
-      },
-    },
-  },
-  {
-    tabBarPosition: 'bottom',
-    animationEnabled: true,
-    configureTransition: (currentTransitionProps, nextTransitionProps) => ({
-      timing: Animated.spring,
-      tension: 1,
-      friction: 35,
-    }),
-    swipeEnabled: false,
-  }
-);
+export default function App() {
+  const store = createStore(rootReducer);
 
-export default TabAnimations;
+  return (
+    <Provider store={store}>
+      <MainNavigation />
+    </Provider>
+  );
+}
---
+++ app/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import reducers from './reducers';
+
+const rootReducer = combineReducers({
+  reducers,
+})
+
+export default rootReducer;
