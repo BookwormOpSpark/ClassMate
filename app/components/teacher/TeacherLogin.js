ae0b9162285068806e865724189ab23c23293300
Gareth <gsveitch@gmail.com>
2018-01-23 16:45:09 -0600

    [feat] reset login stack index upon successfull teacher login

--- app/components/teacher/TeacherLogin.js
+++ app/components/teacher/TeacherLogin.js
@@ -7,6 +7,7 @@ import { Google } from 'expo';
 import { StyleSheet, View } from 'react-native';
 import { connect } from 'react-redux';
 import { Button, Text } from 'react-native-elements';
+import { NavigationActions } from 'react-navigation';
 import { androidClientId, iosClientId, SERVER_URI, TeacherLoginRoute } from '../../constant';
 import { getUser } from '../../actions/actions';
 
@@ -42,7 +43,13 @@ class TeacherLogin extends React.Component {
         .then((res) => {
           const verified = res.payload.id;
           if (verified) {
-            this.props.navigation.navigate('TeacherDrawerNavigation');
+            const resetStack = NavigationActions.reset({
+              index: 0,
+              actions: [
+                NavigationActions.navigate({ routeName: 'TeacherDrawerNavigation' }),
+              ],
+            });
+            this.props.navigation.dispatch(resetStack);
           }
         })
         .catch(err => console.error(err));
