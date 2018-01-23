988259afcdd6144a9e085e5b1954687cf7667424
Hunter Ledet <hunter.ledet@gmail.com>
2018-01-23 16:23:04 -0600

    [feat] added modal access to see student uploaded assignment

--- app/components/teacher/SpecificAssignment.js
+++ app/components/teacher/SpecificAssignment.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import PropTypes from 'prop-types';
-import { StyleSheet, View } from 'react-native';
-import { Text, List, ListItem } from 'react-native-elements';
+import { StyleSheet, View, Modal, Image } from 'react-native';
+import { Text, List, ListItem, Button } from 'react-native-elements';
 import { connect } from 'react-redux';
 
 class SpecificAssignment extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      students: [
-        { name: 'Dorothy Messanger', photoUrl: null },
-        { name: 'Bryan Putz', photoUrl: 'heres a photoUrl' },
-      ],
+      modalVisible: false,
+      currentUrl: null,
     };
   }
+
+  openModal() {
+    this.setState({ modalVisible: true });
+  }
+
+  closeModal() {
+    this.setState({ modalVisible: false });
+  }
+
+
   render() {
     const styles = StyleSheet.create({
       container: {
@@ -41,12 +49,43 @@ class SpecificAssignment extends React.Component {
         marginLeft: 5,
         marginRight: 5,
       },
+      modalContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'grey',
+      },
+      innerContainer: {
+        alignItems: 'center',
+        marginBottom: -650,
+      },
     });
 
     const students = this.props.state.assignmentCheck;
+    const current = this.state.currentUrl;
+    console.log(current, 'this is current photoUrl');
     console.log(this.props.state, 'this is props.state');
     return (
       <View style={{ flex: 1, backgroundColor: '#fff' }}>
+        <Modal
+          visible={this.state.modalVisible}
+          animationType="slide"
+          onRequestClose={() => this.closeModal()}
+        >
+          <View style={styles.modalContainer}>
+            <Image
+              style={{ width: 375, height: 700, justifyContent: 'center', marginTop: -625 }}
+              // need to simply replace URI with variable current to get chosen photoUrl from student
+              source={{ uri: 'http://res.cloudinary.com/fido/image/upload/v1516338431/osxdjtj2mpm9pmhrhbfr.jpg' }}
+            />
+            <View style={styles.innerContainer}>
+              <Button
+                onPress={() => this.closeModal()}
+                title="Close"
+              />
+            </View>
+          </View>
+        </Modal>
         <Text h2 style={styles.container}>Assignment</Text>
         <List style={{ backgroundColor: '#fff' }}>
           {students.map(student => (
@@ -57,6 +96,10 @@ class SpecificAssignment extends React.Component {
                 title={`${student.nameFirst} ${student.nameLast}`}
                 leftIcon={{ name: 'book' }}
                 titleStyle={{ color: 'white' }}
+                onPress={() => {
+                  this.openModal();
+                  this.state.currentUrl = student.photoUrl;
+                }}
               />
             :
               <ListItem
