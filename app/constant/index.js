cbc3e3c23807702c4de8ba96f55f10016191d7f7
Hunter Ledet <hunter.ledet@gmail.com>
2018-01-24 11:33:11 -0600

    [chore] added route to constant/index.js for checkAssignment

--- app/constant/index.js
+++ app/constant/index.js
@@ -1,6 +1,6 @@
 export const androidClientId = '29245857360-gi2ilv6b04e6mpn9icn9ngiq0buq4elr.apps.googleusercontent.com';
 export const iosClientId = '766434726628-1guelram8v9kr9mtapqa2587ss7bc6sl.apps.googleusercontent.com';
-export const SERVER_URI = 'https://5b036ee6.ngrok.io';
+export const SERVER_URI = 'http://3aa4393e.ngrok.io';
 // *********Route************//
 export const TeacherLoginRoute = '/login';
 export const StudentLoginRoute = '/studentCreate';
@@ -16,3 +16,4 @@ export const CreateAssignments = '/createAssignment'; // send session_id => get 
 export const StudentInformation = '/studentInformation'; // send student_id and get back specific student info 
 export const QueueRoute = '/queue'; // send student_id and get back specific student info 
 export const ClassInfoRoute = '/classInfo'; // send student_id and get back specific student info 
+export const CheckAssignment = '/checkAssignment'; // send sessionID and assignmentID - get back list of associated students and their associated HW upload
