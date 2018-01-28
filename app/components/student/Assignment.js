a20ac80f27f75b11d4449877f32477f160d5a691
Hunter Ledet <hunter.ledet@gmail.com>
2018-01-28 10:41:51 -0600

    [style] styled joinClass/addClass and touched up Assignment

--- app/components/student/Assignment.js
+++ app/components/student/Assignment.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import PropTypes from 'prop-types';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, ImageBackground } from 'react-native';
 import { Text, List, ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
-import { blue, white, yellow, orange, red, green } from '../../style/colors.js'
+import blackboard from '../../assets/blackboard.jpg';
+import { blue, white, yellow, orange, red, green } from '../../style/colors';
 import { specificAssignment, selectAssignment } from '../../actions/actions';
 
 class Assignment extends React.Component {
@@ -28,19 +29,24 @@ class Assignment extends React.Component {
   render() {
     const styles = StyleSheet.create({
       container: {
-        backgroundColor: '#fff',
+        backgroundColor: 'transparent',
         alignItems: 'center',
         justifyContent: 'flex-start',
         marginLeft: 110,
       },
       list: {
         borderRadius: 5,
-        borderColor: blue,
-        backgroundColor: blue,
+        borderColor: yellow,
+        backgroundColor: yellow,
         marginTop: 5,
         marginBottom: 5,
         marginLeft: 5,
         marginRight: 5,
+      },
+      contentContainer: {
+        flexGrow: 1,
+        backgroundColor: 'transparent',
+        paddingHorizontal: 10,
       },
     });
 
@@ -49,22 +55,33 @@ class Assignment extends React.Component {
 
 
     return (
-      <View style={{ flex: 1, backgroundColor: '#fff' }}>
+      <ImageBackground
+        source={blackboard}
+        style={{
+          backgroundColor: '#000000',
+          flex: 1,
+          position: 'absolute',
+          width: '100%',
+          height: '100%',
+        }}
+      >
+        <View style={styles.contentContainer}>
 
-        <Text h2 style={{ textAlign: 'center', alignItems: 'center' }}>Assignments</Text>
-        <List style={{ backgroundColor: '#fff' }}>
-          {lessons && lessons.length > 0 ? lessons.map(assignment => (
-            <ListItem
-              containerStyle={styles.list}
-              key={`bbbtn${assignment.id}`}
-              title={`${assignment.title}`}
-              leftIcon={{ name: 'book' }}
-              titleStyle={{ color: 'white' }}
-              onPress={() => this.onSelect(assignment)}
-            />
-        )) : null}
-        </List>
-      </View>
+          <Text h2 style={{ textAlign: 'center', alignItems: 'center', color: yellow }}>Assignments</Text>
+          <List containerStyle={styles.contentContainer}>
+            {lessons && lessons.length > 0 ? lessons.map(assignment => (
+              <ListItem
+                containerStyle={styles.list}
+                key={`bbbtn${assignment.id}`}
+                title={`${assignment.title}`}
+                leftIcon={{ name: 'book', color: 'black' }}
+                titleStyle={{ color: 'black' }}
+                onPress={() => this.onSelect(assignment)}
+              />
+          )) : null}
+          </List>
+        </View>
+      </ImageBackground>
     );
   }
 }
