0591344cd85a5e8b758eabb1cf87af3db0a56e24
Aurelie Lebec <aurelie.lebec@gmail.com>
2018-01-30 12:11:30 -0600

    [badges!]

--- app/components/teacher/BadgeTime.js
+++ app/components/teacher/BadgeTime.js
@@ -1,29 +1,52 @@
 import React from 'react';
+import axios from 'axios';
 import { StyleSheet, View, ImageBackground, Picker } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { connect } from 'react-redux';
+import { Button, Text } from 'react-native-elements';
 import PropTypes from 'prop-types';
 import blackboard from '../../assets/blackboard.jpg';
 import DashHeader from '../shared/Header';
+import { SERVER_URI, SendBadges } from '../../constant';
 
 class BadgeTime extends React.Component {
   constructor(props) {
     super(props);
-    console.log('badge');
-    console.log(this.props.state.classInfo.students);
+    // console.log('badge');
+    // console.log(this.props.state.classInfo.students);
     this.state = { studentSelected: '' };
+    this.postBadge = this.postBadge.bind(this);
   }
+
+  postBadge = async () => {
+    const { students } = this.props.state.classInfo;
+    const student = this.state.studentSelected.split(' ');
+    const studentArr = students.filter(item => item.nameFirst === student[0] && item.nameLast === student[1]);
+    const studentID = studentArr[0].id;
+    const userID = studentArr[0].id;
+
+    console.log(studentID);
+
+    await axios.post(`${SERVER_URI}${SendBadges}`, { badgeType: 'type1', studentID });
+  }
+
   render() {
     const styles = StyleSheet.create({
       container: {
         flex: 1,
         backgroundColor: 'transparent',
         alignItems: 'center',
-        justifyContent: 'center',
+        justifyContent: 'flex-start',
+      },
+      text: {
+        color: 'gold',
+        textAlign: 'center',
       },
     });
     const className = this.props.state.selectSession.sessionName;
     const { students } = this.props.state.classInfo;
+    const { studentSelected } = this.state;
+    console.log(studentSelected);
 
     return (
       <ImageBackground
@@ -47,18 +70,37 @@ class BadgeTime extends React.Component {
             color="gold"
             name="trophy"
             size={100}
-            // onPress={}
             style={styles.icon}
           />
+          <Text h3 style={styles.text}>Good Timing Badge</Text>
           <Picker
-            style={{ width: 100 }}
+            itemStyle={{ color: 'blue', alignSelf: 'center' }}
+            style={{
+              width: 300,
+              backgroundColor: '#FFF0E0',
+              borderColor: 'white',
+              borderWidth: 1,
+              marginTop: 20,
+              color: 'blue',
+            }}
             selectedValue={this.state.studentSelected}
             onValueChange={student => this.setState({ studentSelected: student })}
           >
             {students.map(student => (
-              <Picker.Item label={student.nameFirst} value={student.nameFirst} />
+              <Picker.Item
+                label={`${student.nameFirst} ${student.nameLast}`}
+                value={`${student.nameFirst} ${student.nameLast}`}
+              />
             ))}
           </Picker>
+          <Button
+            buttonStyle={[{ marginBottom: 10, marginTop: 40 }]}
+            title="Give Badge"
+            iconRight={{ name: 'done' }}
+            backgroundColor="#5fad56"
+            borderRadius={5}
+            onPress={this.postBadge}
+          />
         </View>
       </ImageBackground>
     );
